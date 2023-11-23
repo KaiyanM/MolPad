5f12e0972b81485d03d15f87a10bddefebd020a5
KaiyanM <kaiiyenma@gmail.com>
2023-11-23 14:14:59 -0600

    edit description

---
+++ example.R
@@ -0,0 +1,15 @@
+#-----------------------update package----------------------
+library(devtools)
+
+setwd("/Users/hazelma/Documents/GitHub")
+document("MolPad") #important: generate man
+
+install("MolPad")
+library("MolPad")
+
+#-----------------------run example----------------------
+data("test_data")
+
+gDashboard(test_data_processed,test_cluster,test_annotations_processed,test_network,
+           dashboardtitle = "Test",
+           id_colname = c("GO_ID","KEGG_ID"),id_type = c("GO","KEGG"))
